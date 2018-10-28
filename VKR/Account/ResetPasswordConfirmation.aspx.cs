187c7e46e76eef37e796c95a1ba2a59d2603301a
Фархат <epalzyorinchos@mail.ru>
2018-10-28 19:38:52 +0600

    Добавьте файлы проекта.

---
+++ VKR/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace VKR.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ VKR/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VKR.Startup))]
+namespace VKR
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
